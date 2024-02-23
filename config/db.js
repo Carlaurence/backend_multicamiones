8ece059913b95d30024643f2887b9d3c79e19583
Carlaurence <ingeniero.indbermeo@gmail.com>
2024-02-23 15:03:50 -0500

    Agregando el file  middleware allowCors de vercel en el post del router Product

--- config/db.js
+++ config/db.js
@@ -11,9 +11,6 @@ const connectionDB = async() => {
             }
         );
 
-        const url = `${stringConnection.connection.host}:${stringConnection.connection.port}`;
-        //console.log(`MongoDB Conectado en :${url}`);
-        //console.log(`MongoDB Conectado en : ${dbConfig.uri}${dbConfig.name}`);
         console.log('MongoDB Multicomerciales_V2 conectado...')
 
     }catch(error){
